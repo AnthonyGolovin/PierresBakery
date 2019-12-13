38070dccff66eaeb23f0f01253651baceaa3a6d4
Anthony Golovingit config --global user.name Anthony <AGOLOVIN97@live.com>
2019-12-13 09:11:42 -0800

    Create and added pastry and bread content and constructors

--- Products/Bread.cs
+++ Products/Bread.cs
@@ -1,3 +1,25 @@
-  
 using System;
 using System.Collections.Generic;
+
+namespace Product.Bread
+{
+    class Bread
+    {
+        public int BreadQuantity { get; set; }
+        public int BreadCost { get; set; }
+        public int BreadTotal { get; set; }
+
+        public Bread(int breadQuantity, int breadCost, int breadTotal)
+        {
+            BreadQuantity = breadQuantity;
+            BreadCost = 5;
+            BreadTotal =
+        }
+
+        public int BreadCalculation()
+        {
+            int 
+        }
+    }
+
+}
---
+++ Products/Pastry.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Collections.Generic;
+
+namespace Product.Pastry
+{
+    class Bread
+    {
+        public int PastryQuantity { get; set; }
+        public int PastryCost { get; set; }
+        public int PastryTotal { get; set; }
+
+        public Bread(int pastryQuantity, int pastryCost, int pastryTotal)
+        {
+            PastryQuantity = breadQuantity;
+            PastryCost = 5;
+            PastryTotal =
+        }
+
+        public int PastryCalculation()
+        {
+            int 
+        }
+    }
+
+}
