75cd4e67cadfffe858d836c2bd382bd9cf212bf5
Anthony Golovingit config --global user.name Anthony <AGOLOVIN97@live.com>
2019-12-13 11:17:32 -0800

    Start front end work and research console styling

--- PierresBakery.cs
+++ PierresBakery.cs
@@ -8,7 +8,14 @@ namespace Bakery {
     {
         public static void Main()
         {
+            Console.WriteLine("BAKERY PRODUCTS:");
+            Console.WriteLine("Bread: $5 per piece.");
+            Console.WriteLine("Pastry: $2 per pastry.");
+            Console.WriteLine("Enter the amount of Bread and Pastries you would like in the following format (Bread Quanity. Pastry Quantity) EX.(5,9).");
+            int userInput = int.Parse(Console.ReadLine());
+            int[] cart = userInput.Split(",");
 
+            Console.WriteLine("");
         }
     }
 }
