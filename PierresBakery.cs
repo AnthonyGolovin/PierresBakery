63f068dffef6640ad56da21a041c0ffaa497c6b3
Anthony Golovingit config --global user.name Anthony <AGOLOVIN97@live.com>
2019-12-13 13:15:48 -0800

    Implement more DRY code as well as created some error hanlding in case the user inputs information in the inccorrect format

--- PierresBakery.cs
+++ PierresBakery.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.Threading;
 using Product.Bread;
 using Product.Pastry;
 
@@ -8,24 +9,37 @@ namespace Bakery {
     {
         public static void Main()
         {
-            Console.WriteLine("BAKERY PRODUCTS:");
+            Bakery();
+        }
+        public static void Bakery()
+            {
+            Console.WriteLine("\nBAKERY PRODUCTS:");
             Console.WriteLine("Bread: $5 per piece.");
             Console.WriteLine("Pastry: $2 per pastry.");
-            Console.WriteLine("Enter the amount of Bread and Pastries you would like in the following format (Bread Quanity. Pastry Quantity) EX.(5,9).");
+            Console.WriteLine("\n\nEnter the amount of Bread and Pastries you would like in the following format (Bread Quanity. Pastry Quantity) EX.(5,9).\n");
+                
                 string userInput = Console.ReadLine();
                 string[] cart = userInput.Split(",");
+                try {
                 int amountOfBread = int.Parse(cart[0]);
                 int amountOfPastry = int.Parse(cart[1]);
 
                 Bread bread = new Bread(amountOfBread);
-                    Console.WriteLine("$" + bread.BreadTotal + " is the total for the Bread");
+                    Console.WriteLine("\n$" + bread.BreadTotal + " is the total for the Bread");
 
                 Pastry pastry = new Pastry (amountOfPastry);
                      Console.WriteLine("$" + pastry.PastryTotal + " is the total for the Pastry");
 
                 int totalTotal = bread.BreadTotal + pastry.PastryTotal;
 
-                Console.WriteLine("Your total for today will be $"  + totalTotal);
-        }
+                Console.WriteLine("\nYour total for today will be $"  + totalTotal + "\n");
+                } catch (Exception error) //Exception is a data type for error handling and error is the name used if you wanna print it or something (Note to self)
+                {
+                    Console.WriteLine("\nINVALID FORMAT, PLEASE USE NUMBERS AND CREATE YOU ORDER IN THE EXAMPLE FORMAT: EX.5,5\n");
+                    Thread.Sleep(3000);
+                    Bakery();
+
+                }
+            }
     }
 }
--- Products/Pastry.cs
+++ Products/Pastry.cs
@@ -10,7 +10,7 @@ namespace Product.Pastry
         public int PastryTotal { get; set; }
         public int PastryDiscount { get; set; }
 
-        public Pastry(int pastryQuantity) //Dont add pastry cost in arguements its just gonna be over written by the constructor price
+        public Pastry(int pastryQuantity) //Dont add pastry cost in arguements its just gonna be over written by the constructor price (Note to self)
         {
             PastryQuantity = pastryQuantity;
             PastryCost = 2;
